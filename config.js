24c7dd0471647dc238756befbc8881012b449c0d
樊昊 <41361820+hao-F@users.noreply.github.com>
2022-01-27 14:32:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  sessionid: '7a95d20916fb3458a375d66e50ede2df',
-  aid: '2608',
-  uuid: '7056690055519847951',
-  _signature: '_02B4Z6wo00101Yh.CxAAAIDBCH3xUk.B2QmIew-AAAPuxYbtBmhNbsxqPgJM.unKL5Y83B6bHbi93yYIbWNdpU6shPY57EZ62bis0Zy6hNA3y.lw4B8q0j7lK2z4cqMncZ9aKE8c7lhAkyxqab'
+  sessionid: '',
+  aid: '',
+  uuid: '',
+  _signature: ''
 }
