efc297b812a0bc69868cd57791d12276a3bc4f5a
fanhao <846186994@qq.com>
2022-01-27 15:31:55 +0800

    解决冲突

--- config.js
+++ config.js
@@ -1,33 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
-  sessionid: '7a95d20916fb3458a375d66e50ede2df',
-  aid: '2608',
-  uuid: '7056690055519847951',
-  _signature: '_02B4Z6wo00101Yh.CxAAAIDBCH3xUk.B2QmIew-AAAPuxYbtBmhNbsxqPgJM.unKL5Y83B6bHbi93yYIbWNdpU6shPY57EZ62bis0Zy6hNA3y.lw4B8q0j7lK2z4cqMncZ9aKE8c7lhAkyxqab',
-  cookieList: `
-                _tea_utm_cache_2608={%22utm_source%22:%22gold_browser_extension%22}; 
-                __tea_cookie_tokens_2608=%257B%2522web_id%2522%253A%25227056690055519847951%2522%252C%2522ssid%2522%253A%2522f5b3a1b9-1042-44df-a95f-fea29ab81b4f%2522%252C%2522user_unique_id%2522%253A%25227056690055519847951%2522%252C%2522timestamp%2522%253A1643013701461%257D; 
-                _ga=GA1.2.1050322222.1643250959; 
-                _gid=GA1.2.837794154.1643250959; 
-                passport_csrf_token_default=61fc4a8d13bd2e6f784d9e739cdae20a; 
-                passport_csrf_token=61fc4a8d13bd2e6f784d9e739cdae20a; 
-                _tea_utm_cache_2018={%22utm_source%22:%22gold_browser_extension%22}; 
-                sid_guard=069b382961d0c707ac881655ad47fc85%7C1643250983%7C5184000%7CMon%2C+28-Mar-2022+02%3A36%3A23+GMT; 
-                uid_tt=673356ceea70a6485391227005d306ce; 
-                uid_tt_ss=673356ceea70a6485391227005d306ce; 
-                sid_tt=069b382961d0c707ac881655ad47fc85; 
-                sessionid=069b382961d0c707ac881655ad47fc85; 
-                sessionid_ss=069b382961d0c707ac881655ad47fc85; 
-                sid_ucp_v1=1.0.0-KGJhZDEwMjg0YzUwOTExNGY0MGZkOGFhZTc1OWM3YWY2Mjc0OGEyODYKFwi41NDA_fXRBRCnisiPBhiwFDgCQO8HGgJsZiIgMDY5YjM4Mjk2MWQwYzcwN2FjODgxNjU1YWQ0N2ZjODU; 
-                ssid_ucp_v1=1.0.0-KGJhZDEwMjg0YzUwOTExNGY0MGZkOGFhZTc1OWM3YWY2Mjc0OGEyODYKFwi41NDA_fXRBRCnisiPBhiwFDgCQO8HGgJsZiIgMDY5YjM4Mjk2MWQwYzcwN2FjODgxNjU1YWQ0N2ZjODU; 
-                n_mh=1fSfIwXe0PkvRIDG7wlphCo1kMv53Hs2RrrCRTv__P8; 
-                MONITOR_WEB_ID=1cc0737f-858f-4680-9603-e6c9ef8eb2d0
-              `
-}
-=======
   sessionid: '',
   aid: '',
   uuid: '',
-  _signature: ''
+  _signature: '',
+  cookieList
 }
->>>>>>> 24c7dd0471647dc238756befbc8881012b449c0d
